47a2a6ded7e4fbc08c72a87b79c9d2da67f22562
Tanya Ivato <tetiana.ivato@gmail.com>
2024-07-11 13:42:44 +0300

    Configure Vite and React Router for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ims">
       <App />
     </BrowserRouter>
   </React.StrictMode>
